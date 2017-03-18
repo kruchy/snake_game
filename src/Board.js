281972fa48206062b56297643e0159e9c85075c2
Kruchy <kruchy61@gmail.com>
2017-03-18 13:15:31 +0100

    Create stylesheet

--- src/Board.js
+++ src/Board.js
@@ -3,14 +3,26 @@
  */
 import React, {Component, PropTypes} from "react";
 import Vector from "./Vector";
+import _ from "underscore";
+import "./Board.css";
+
 export default class Board extends Component {
     static propTypes =
         {
-            size: PropTypes.instanceOf(Vector)
+            size: PropTypes.instanceOf(Vector),
+            snakePositions: PropTypes.arrayOf(PropTypes.instanceOf(Vector)),
+            fruitPosition: PropTypes.instanceOf(Vector)
         };
 
     render() {
-        return <div>{this.props.size.x}x{this.props.size.y}</div>
+        const rows = _.range(this.props.size.y).map(y => {
+            const map = _.range(this.props.size.x).map(x => {
+                return <div className="cell"></div>
+            });
+            return <div className="row">{map}</div>;
+        });
+
+        return <div className="Board">{rows}</div>;
 
     }
 
